b2096f0af3dcafcc66ec60f7e2865d6676e56a0b
Aleš Kalan <slo.runner.samp@gmail.com>
2017-07-12 20:53:22 +0200

    Added some new features

---
+++ config.js
@@ -0,0 +1,44 @@
+module.exports = {
+
+	admin: {
+		Oshane: '76561198103757293'
+	},
+
+	bot: {
+		name: '',
+		username: '',
+		password: '',
+		identity_secret: '',
+		shared_secret: '',
+		steam_id: '',
+		apikey: '',
+		tradelink: '',
+		apicode: '96195df55ea16a90b0d45b2bdff7b4ac85f1f21a'
+	},
+
+	message: {
+		welcome: '',
+		help: '',
+		buy: '',
+		tradeoffer: '',
+		cards_sold: '',
+		invalid_command: '',
+		invalid_number_of_keys: '',
+		excess_keys: '',
+		not_in_friendlist: ''
+	},
+
+	max_number_of_keys: 15,
+
+	sets_per_key: 16,
+
+	app_id: {
+		csgo: 730,
+		steam: 753
+	},
+
+	context_id: {
+		keys: 2,
+		cards: 6
+	}
+};
